024686e500d741c1ffd40c26f2a97de78a1aa39b
Assan Kozhin <kozhin.asan@gmail.com>
2023-05-27 15:19:45 +0800

    migrate store.cpp

--- src/bananadb/store.cpp
+++ src/bananadb/store.cpp
@@ -1,6 +1,39 @@
-#include <iostream>
-#include "store.h"
+#include "./store.h"
 
-void helloFromDB() {
-    std::cout << "hello from db" << std::endl;
+#include <map>
+#include <string>
+#include <vector>
+
+bool Store::Set(const std::string& key, const std::string& val) {
+    const auto it= this->store.insert({key, val});
+    return it.second;
+}
+
+std::pair<std::string, bool> Store::Get(const std::string& key) {
+        const auto it = this->store.find(key);
+        if(it == this->store.end()) {
+            return std::make_pair("", false);
+        }
+        return std::make_pair(it->second, true);
+}
+
+bool Store::DeleteByKey(const std::string& key) {
+    return this->store.erase(key);
+}
+
+std::vector<std::string> Store::GetAllKeys() {
+    std::vector<std::string> keys;
+    for(auto & it : this->store) {
+        keys.push_back(it.first);
+    }
+    return keys;
+}
+
+std::string Store::GetSet(const std::string &key, const std::string &default_val) {
+    auto it = this->store.find(key);
+    if(it==this->store.end()) {
+        this->Set(key, default_val);
+    }
+    // Using [] operator since value exists no matter what
+    return this->store[key];
 }
--- src/bananadb/store.h
+++ src/bananadb/store.h
@@ -1,6 +1,21 @@
-#ifndef store
-#define store
+#ifndef _STORE_H_
+#define _STORE_H_
 
-void helloFromDB();
+#include <map>
+#include <string>
+#include <vector>
+
+class Store {
+private:
+    std::map<std::string, std::string> store;
+public:
+    // Set method returns true if insertion is successful, otherwise it return false.
+    bool Set(const std::string& key, const std::string& val);
+    // This method return std::pair<value, true>. If key does not exist in map it returns std::pair<"", false>.
+    std::pair<std::string, bool> Get(const std::string& key);
+    bool DeleteByKey(const std::string& key);
+    std::vector<std::string> GetAllKeys();
+    std::string GetSet(const std::string& key, const std::string& default_val);
+};
 
 #endif
